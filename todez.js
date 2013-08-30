ddee3540ea5ee6d048d2faedcc345f80edc7c146
Patrick Looser <patrick.looser@kaba.com>
2013-08-30 15:13:55 +0200

    file optional

--- todez.js
+++ todez.js
@@ -1,10 +1,10 @@
 #! /usr/bin/env node
 
-console.log("todez:");
 if (process.argv.length <= 2) {
   console.log('todez version: ' + require('./package.json').version);
-  console.log('use "$ todez <hex number>" to convert do dezimal byte style array');
-  console.log('use "$ todez <hex number>" -o to open the reult in notepad)');
+  console.log('use "$ todez <hex number>" to convert to decimal byte style array');
+  console.log('use "$ todez <hex number>" -f to write the result to a file)');
+  console.log('use "$ todez <hex number>" -fo to open the result.txt file in notepad)');
 } else {
   var key = process.argv[2];
   if (key.length % 2) {
@@ -15,14 +15,16 @@ if (process.argv.length <= 2) {
       hexNumbers.push(parseInt((key[i] + key[i+1]), 16));
     }
     console.log('\n' + hexNumbers + '\n');
-    require('fs').writeFile('result.txt', hexNumbers, function (err) {
-      if (err) {
-        console.log(err);
-      } else if (process.argv.length > 3) {
-        require('child_process').exec('notepad.exe result.txt');
-      } else {
-        console.log('finished --> see file: result.txt');
-      }
-    });
+    if (process.argv.length > 3 && process.argv[3].length > 0) {
+      require('fs').writeFile('result.txt', hexNumbers, function (err) {
+        if (err) {
+          console.log(err);
+        } else if (process.argv[3].length > 2) {
+          require('child_process').exec('notepad.exe result.txt');
+        } else {
+          console.log('finished --> see file: result.txt');
+        }
+      });
+    }
   }
 }
